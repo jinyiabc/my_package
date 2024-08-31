773a8ebe780c3c0430575fe428064d3d4d7b71f0
jinyi <jinyiabc@gmail.com>
2024-08-31 11:01:04 +0800

    First Commit

---
+++ launch/listener_launch.py
@@ -0,0 +1,9 @@
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_nodes_py',
+            executable='listener'
+        )
+    ])
